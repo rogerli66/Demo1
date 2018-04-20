57686aace8e7f7bc717c0c77d8d462263c42a240
Roger Li <37480615+rogerli66@users.noreply.github.com>
2018-04-20 18:30:02 -0500

    Update HelloDate.java

--- HelloDate.java
+++ HelloDate.java
@@ -18,7 +18,7 @@ public class HelloDate {
      */
     public static void main (String[] args) {
 	System.out.println ("Hello, it's: ");
-	System.out.println(new Date());
+	System.out.println(new Date() +"of the year.");
     }
 }
 
